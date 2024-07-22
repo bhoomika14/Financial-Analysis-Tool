123c5282c11ee8abee004976112e163f07c55913
Bhoomika <bhoomikaudaykumar99@gmail.com>
2024-07-22 17:20:39 +0530

    Prompts updated

--- llm_insights.py
+++ llm_insights.py
@@ -1,13 +1,15 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 import json
+import streamlit as st
 
 with open('config.json') as f:
     config = json.load(f)
 os.environ["GOOGLE_API_KEY"] = config["GOOGLE_API_KEY"]
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
+@st.cache_resource
 def llm_data(data):
-    insights = llm.invoke(f"{data} is a time series data. Provide your insights by analyzing the data.")
+    insights = llm.invoke(f"{data} is a time series data. Provide your insights by analyzing the data. Your insights should also include information about Trend and Seasonality.")
 
     return insights.content
