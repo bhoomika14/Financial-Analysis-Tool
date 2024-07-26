59e50437c4d3bf3cedf9453cd362ce528e7f2dc9
Bhoomika <bhoomikaudaykumar99@gmail.com>
2024-07-26 17:55:45 +0530

    Insights using LLM

--- llm_insights.py
+++ llm_insights.py
@@ -1,4 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain import PromptTemplate
 import os
 import json
 import streamlit as st
@@ -6,10 +7,19 @@ import streamlit as st
 with open('config.json') as f:
     config = json.load(f)
 os.environ["GOOGLE_API_KEY"] = config["GOOGLE_API_KEY"]
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
+llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.0)
 
 @st.cache_resource
 def llm_data(data):
-    insights = llm.invoke(f"{data} is a time series data. Provide your insights by analyzing the data. Your insights should also include information about Trend and Seasonality.")
+    insights = llm.invoke(f"""{data} is a financial data. The data represents the monthly sales of medicines for the past several years. 
+                          The data includes DATE column and AMOUNT column. Analyse the data and provide your insights into the following aspects of this data:<br>
+                          1. Seasonal Trends: What seasonal patterns or trends are visible in the sales data?
+
+                          2. Trend Analysis: What are the long-term trends in the sales data? Are sales generally increasing, decreasing, or remaining stable over time?
+
+                          3. Anomalies and Outliers: Are there any significant anomalies or outliers in the sales data?
+
+                          4. Forecasting: Based on historical data, what are the projected sales figures for the upcoming months or years? Are there any forecasts or predictions that can be made?</br>
+                          """)
 
     return insights.content
